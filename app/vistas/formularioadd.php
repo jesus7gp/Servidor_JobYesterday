6297116f150c13db9924f3aec9bc2ae33e8c3040
jesus7gp <jgonzalezpacheco1112@gmail.com>
2016-11-05 21:47:39 +0100

    Corregido fallo de formularioadd

--- app/vistas/formularioadd.php
+++ app/vistas/formularioadd.php
@@ -25,7 +25,8 @@
 			<input type="text" name="descripcion" class="form-control" value="<?=VP('descripcion','')?>"><br>
 		</fieldset>
 		<fieldset class="form-group">
-			<label for=""text" name="perscont" class="form-control" value="<?=VP('perscont','')?>"><br>
+			<label for="">Persona de contacto</label>
+			<input for=""text" name="perscont" class="form-control" value="<?=VP('perscont','')?>"><br>
 		</fieldset>
 		<fieldset class="form-group">
 			<label for="">Teléfono de contacto:</label>
